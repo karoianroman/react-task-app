650953f6b75356c0c8289aa7df476a861d22522c
karoianroman <romashka99k@gmail.com>
2026-04-03 09:50:13 +0300

    commit

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,10 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  // Додаємо відносні шляхи, щоб index.html правильно знаходив JS/CSS у папці /assets
+  base: './', 
+  build: {
+    // Явно вказуємо папку збірки (має збігатися з тим, що в Dockerfile)
+    outDir: 'dist',
+  }
 });
