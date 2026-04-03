cec910b745783b9e3e482a88c341001ef4a64555
karoianroman <romashka99k@gmail.com>
2026-04-03 10:06:53 +0300

    commit

--- vite.config.js
+++ vite.config.js
@@ -3,15 +3,13 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: './',
+  base: './', // Робить шляхи відносними (вкрай важливо для Nginx)
+  server: {
+    host: true,
+    port: 8080
+  },
   build: {
     outDir: 'dist',
-    rollupOptions: {
-      output: {
-        entryFileNames: 'assets/[name]-[hash].js',
-        chunkFileNames: 'assets/[name]-[hash].js',
-        assetFileNames: 'assets/[name]-[hash].[ext]'
-      }
-    }
+    emptyOutDir: true
   }
 });
